63621925f4aebcd80da1c1271d1df795782def3e
Luan Custódio <luancustodio@sou.faccat.br>
2025-08-21 22:17:19 -0300

    users crud start

--- Atividades/G1/api-carros/routes/usersRoutes.js
+++ Atividades/G1/api-carros/routes/usersRoutes.js
@@ -1,12 +1,30 @@
 const express = require('express')
+let usersData = require('../data/usersData')
 const router = express.Router()
 
 router.route('/users')
-    .get((req, res) => res.send("Get all users."))
-    .post((req, res) => res.send("New user saved."))
+    .get((req, res) => {
+        res.json(usersData)
+    })
+    .post((req, res) => {
+        const newUser = {
+            id: usersData.length + 1,
+            "name": req.body.name,
+            "password": req.body.password,
+            "userType": req.body.userType,
+        }
+        usersData.push(newUser)
+        res.status(201).json(newUser)
+    })
 
 router.route('/users/:id')
-    .get((req, res) => res.send("Get user by id"))
+    .get((req, res) => {
+        const id = parseInt(req.params.id)
+        const user = usersData.find(user => user.id === id)
+        if(!user)
+            return res.status(404).json({error: 'User not found'})
+        res.json(user)
+    })
     .put((req, res) => res.send("Updated user"))
     .delete((req, res) => res.send("User deleted"))
 
